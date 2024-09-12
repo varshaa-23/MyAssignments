0f8a851f9e88760930adb55f8732784b2237c7d7
varshaa-23 <varshaakalai@gmail.com>
2024-09-12 09:29:35 +0530

    FindOddNumbers

---
+++ FindOddNumbers.java
@@ -0,0 +1,21 @@
+package week1.day3;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+
+		int maxRange=10;
+
+		for(int i=1;i<=maxRange;i++) {
+
+
+			if(i%2==1) {
+				System.out.println(i);
+			}
+
+		}
+
+
+	}
+
+}
